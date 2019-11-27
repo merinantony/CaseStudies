d9012c5fa00331b6a4505745aac229e71a633bcb
USER <USER@FACE-88>
2019-11-27 18:48:28 +0530

    casestudy1 casestudy2 encapsulation inheritance inheritance case study multilevel inheritance oops registration

---
+++ case study/src/casestudy2/Booking.java
@@ -0,0 +1,14 @@
+package casestudy2;
+
+import java.util.Scanner;
+
+public class Booking {
+
+	public void register() {
+		// TODO Auto-generated method stub
+		
+		int book[]=new int[25];
+		Scanner sc=new Scanner(System.in);
+}
+
+}
